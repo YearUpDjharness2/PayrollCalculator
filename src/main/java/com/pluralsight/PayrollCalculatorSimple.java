53e1e6373a0f12ed89be42106e668327406ada39
Djharness <djharness2@gmail.com>
2024-09-30 15:57:53 -0400

    Completed Mod3 - Exercise 4 - Payroll Calculator

---
+++ src/main/java/com/pluralsight/PayrollCalculatorSimple.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculatorSimple {
+    public static void main(String[] srgs){
+        Scanner scanner = new Scanner(System.in);
+
+        //prompt for name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        //prompt for hours worked
+        System.out.print("Enter hours worked: ");
+        float hoursWorked = scanner.nextFloat();
+
+        //prompt for pay rate
+        System.out.print("Enter pay rate: ");
+        float payRate = scanner.nextFloat();
+
+        //calculate
+        float grosspay = hoursWorked * payRate;
+
+        //display
+        System.out.printf("Employee Name; %s%n ",name);
+        System.out.printf("Gross Pay: $%.2f%n ",grosspay);
+
+
+
+    }
+
+}
+
